70ea94e6e5aa8990bbdc125d20c4bf674aae3037
mdim2379 <molnardavid0424@gmail.com>
2025-03-06 09:35:53 +0200

    arrayek visszaallitasa

--- Program.cs
+++ Program.cs
@@ -107,26 +107,24 @@ namespace Szeminarium1
             uint vao = Gl.GenVertexArray();
             Gl.BindVertexArray(vao);
 
-            //haromszogek elrontasa, tombok elrontasa
+            //minden visszallitasa
             float[] vertexArray = new float[] {
-                -0.5f, -5f, 0.0f,
+                -0.5f, -0.5f, 0.0f,
                 +0.5f, -0.5f, 0.0f,
-                0.0f, +0.5f, 0.0f,
-                1f, 1f
+                 0.0f, +0.5f, 0.0f,
+                 1f, 1f, 0f
             };
 
-            //szinek tomb elrontasa
             float[] colorArray = new float[] {
-                1.0f, 0.0f, 0.0f, 70f,
+                1.0f, 0.0f, 0.0f, 1.0f,
                 0.0f, 1.0f, 0.0f, 1.0f,
-                0.0f, 14.0f, 1.0f, 100f,
+                0.0f, 0.0f, 1.0f, 1.0f,
                 1.0f, 0.0f, 0.0f, 1.0f,
             };
 
-            //index tomb elrontasa
             uint[] indexArray = new uint[] { 
-                1, 2, 3,
-                3, 2, 1
+                0, 1, 2,
+                2, 1, 3
             };
 
             uint vertices = Gl.GenBuffer();
