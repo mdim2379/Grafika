616077a26385aca626745966ea686dcd7885a332
mdim2379 <molnardavid0424@gmail.com>
2025-03-12 21:22:17 +0200

    attachshader torlese, fehet kep

--- Program.cs
+++ Program.cs
@@ -75,9 +75,9 @@ namespace Szeminarium1
             Gl.CompileShader(fshader);
 
             program = Gl.CreateProgram();
-            Gl.AttachShader(program, vshader);
-            Gl.AttachShader(program, fshader);
-           // Gl.LinkProgram(program);
+           // Gl.AttachShader(program, vshader);
+           // Gl.AttachShader(program, fshader);
+            Gl.LinkProgram(program);
             Gl.DetachShader(program, vshader);
             Gl.DetachShader(program, fshader);
             Gl.DeleteShader(vshader);
